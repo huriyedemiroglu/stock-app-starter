5d0c74b34034a5e6d626d963a60a8bdbdbe69bf8
“Huriye_Dgit config --global user.email “demirogluhu@gmail.com <demirogluhu@gmail.com>
2023-08-31 14:12:22 -0500

    axios updated with axios-instance

--- src/hooks/useStockCall.jsx
+++ src/hooks/useStockCall.jsx
@@ -4,19 +4,23 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchStart } from "../features/stockSlice";
 import { getSuccess } from "../features/stockSlice";
 import { fetchFail } from "../features/stockSlice";
+import useAxios from './useAxios';
+
 const useStockCall = () => {
   const dispatch =useDispatch()
   const {token}=useSelector((state)=>state.auth)
+  const {axiosWithToken}=useAxios()
   
   const getStockData = async(url)=>{
-    const BASE_URL= "https://10001.fullstack.clarusway.com/"
+    //const BASE_URL= "https://10001.fullstack.clarusway.com/"
   
     dispatch(fetchStart())
    
     try{
-    const {data} = await axios(`${BASE_URL}stock/${url}/`,{
-      headers: {Authorization: `Token ${token}`},
-    })
+    // const {data} = await axios(`${BASE_URL}stock/${url}/`,{
+    //   headers: {Authorization: `Token ${token}`},
+    // })
+    const {data} =  await axiosWithToken.get(`stock/${url}/`)
     dispatch(getSuccess({data,url}))
   }catch(error){
     console.log(error)
@@ -24,14 +28,9 @@ const useStockCall = () => {
   }
     }
     const deleteStockData = async(url,id)=>{
-      const BASE_URL= "https://10001.fullstack.clarusway.com/"
-    
       dispatch(fetchStart())
-     
-      try{
-       await axios.delete(`${BASE_URL}stock/${url}/${id}/`,{
-        headers: {Authorization: `Token ${token}`},
-      })
+     try{
+       await axiosWithToken.delete(`stock/${url}/${id}/`)
      getStockData(url)
     }catch(error){
       console.log(error)
