e372d82fe880a759db6b46814bc4da201d71ce23
“Huriye_Dgit config --global user.email “demirogluhu@gmail.com <demirogluhu@gmail.com>
2023-10-20 13:04:45 -0500

    home page kpi

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,27 @@
-import { Typography } from "@mui/material";
+import { Typography } from "@mui/material"
+import { useEffect } from "react"
 import Charts from "../components/Charts"
 import KpiCards from "../components/KpiCards"
+import useStockCalls from "../hooks/useStockCalls"
 
 const Home = () => {
+  const { getStockData } = useStockCalls()
+
+  useEffect(() => {
+    getStockData("sales")
+    getStockData("purchases")
+  }, [])
+
   return (
-  <div>
-    <Typography variant="h4" color="error" mb={3}>
-      Dashboard
-    </Typography>
-    <KpiCards/>
-    <Charts/>
+    <div>
+      <Typography variant="h4" color="error" mb={3}>
+        Dashboard
+      </Typography>
+
+      <KpiCards />
+      <Charts />
     </div>
-    )
+  )
+}
 
-};
-
-export default Home;
+export default Home
